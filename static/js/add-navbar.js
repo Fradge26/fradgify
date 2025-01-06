3adf0269a6d4d275dd0e914d1e52deee607fadbc
Adam Fardgley <fradge@hotmail.co.uk>
2025-01-06 00:14:04 +0000

    navbar

--- static/js/add-navbar.js
+++ static/js/add-navbar.js
@@ -34,3 +34,18 @@ document.addEventListener("DOMContentLoaded", () => {
     // Insert the navbar at the top of the body
     document.body.insertBefore(navbar, document.body.firstChild);
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+});
